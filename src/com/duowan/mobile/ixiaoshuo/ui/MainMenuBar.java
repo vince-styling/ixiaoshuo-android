3db5ecd00aed60c1d7e0da7a4d3ed8cbb72107c2
vince <lingyunxiao@qq.com>
2013-08-23 20:11:35 +0800

    完善底部全局菜单的绘制

--- src/com/duowan/mobile/ixiaoshuo/ui/MainMenuBar.java
+++ src/com/duowan/mobile/ixiaoshuo/ui/MainMenuBar.java
@@ -6,9 +6,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.NinePatchDrawable;
 import android.util.AttributeSet;
 import android.util.SparseArray;
+import android.view.MotionEvent;
+import android.view.SoundEffectConstants;
 import android.view.View;
 import com.duowan.mobile.ixiaoshuo.R;
 import com.duowan.mobile.ixiaoshuo.reader.BaseActivity;
@@ -32,34 +33,31 @@ public class MainMenuBar extends View {
 
 	private int mPadding;
 	private int mSelectedItemId;
+	private Drawable mHighlightDrawable;
 
 	private void init() {
 		mMenuItems = new SparseArray<MenuItem>(4);
 
-		mMenuItems.put(MENU_BOOKSHELF, new MenuItem(R.drawable.menu_bookshelf_on, R.drawable.menu_bookshelf_off, new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+		mMenuItems.put(MENU_BOOKSHELF, new MenuItem(R.drawable.menu_bookshelf_on, R.drawable.menu_bookshelf_off, new ClickEvent() {
+			public void onClick() {
 				getActivity().showToastMsg("点击了书架菜单");
 			}
 		}));
 
-		mMenuItems.put(MENU_FINDER, new MenuItem(R.drawable.menu_finder_on, R.drawable.menu_finder_off, new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+		mMenuItems.put(MENU_FINDER, new MenuItem(R.drawable.menu_finder_on, R.drawable.menu_finder_off, new ClickEvent() {
+			public void onClick() {
 				getActivity().showToastMsg("点击了发现菜单");
 			}
 		}));
 
-		mMenuItems.put(MENU_DETECT, new MenuItem(R.drawable.menu_detect_on, R.drawable.menu_detect_off, new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+		mMenuItems.put(MENU_DETECT, new MenuItem(R.drawable.menu_detect_on, R.drawable.menu_detect_off, new ClickEvent() {
+			public void onClick() {
 				getActivity().showToastMsg("点击了雷达菜单");
 			}
 		}));
 
-		mMenuItems.put(MENU_SEARCH, new MenuItem(R.drawable.menu_search_on, R.drawable.menu_search_off, new OnClickListener() {
-			@Override
-			public void onClick(View v) {
+		mMenuItems.put(MENU_SEARCH, new MenuItem(R.drawable.menu_search_on, R.drawable.menu_search_off, new ClickEvent() {
+			public void onClick() {
 				getActivity().showToastMsg("点击了搜索菜单");
 			}
 		}));
@@ -79,9 +77,11 @@ public class MainMenuBar extends View {
 			MenuItem menuItem = mMenuItems.get(itemId);
 
 			if (itemId == mSelectedItemId) {
-				Drawable highlightDrawable = getResources().getDrawable(R.drawable.menu_bg_pressed);
-				highlightDrawable.setBounds(0, 0, itemWidth, getHeight());
-				highlightDrawable.draw(canvas);
+				if (mHighlightDrawable == null) {
+					mHighlightDrawable = getResources().getDrawable(R.drawable.menu_bg_pressed);
+					mHighlightDrawable.setBounds(0, 0, itemWidth, getHeight());
+				}
+				mHighlightDrawable.draw(canvas);
 			}
 
 			Bitmap iconBitmap = BitmapFactory.decodeResource(getResources(),
@@ -89,32 +89,51 @@ public class MainMenuBar extends View {
 
 			int iconDstWidth = (int) (iconDstHeight * ((double) iconBitmap.getWidth() / iconBitmap.getHeight()));
 
-			Rect srcRect = new Rect();
-			srcRect.left = 0;
-			srcRect.top = 0;
-			srcRect.right = iconBitmap.getWidth();
-			srcRect.bottom = iconBitmap.getHeight();
-
 			Rect dstRect = new Rect();
 			dstRect.left = (itemWidth - iconDstWidth) / 2;
-			dstRect.top = 0;
+			dstRect.top = mPadding;
 			dstRect.right = dstRect.left + iconDstWidth;
-			dstRect.bottom = iconDstHeight;
+			dstRect.bottom = mPadding + iconDstHeight;
 
-			canvas.drawBitmap(iconBitmap, srcRect, dstRect, null);
-			canvas.translate(itemWidth, mPadding);
+			canvas.drawBitmap(iconBitmap, null, dstRect, null);
+			canvas.translate(itemWidth, 0);
 		}
 		canvas.restore();
 	}
 
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_UP:
+				if (event.getY() > 0) {
+					int itemWidth = getWidth() / mMenuItems.size();
+					int itemIndex = (int) (event.getX() / itemWidth);
+					int itemId = mMenuItems.keyAt(itemIndex);
+					if (itemId != mSelectedItemId) {
+						MenuItem menuItem = mMenuItems.get(itemId);
+						menuItem.clickEvent.onClick();
+						mSelectedItemId = itemId;
+						invalidate();
+						playSoundEffect(SoundEffectConstants.CLICK);
+					}
+				}
+				return true;
+		}
+		return true;
+	}
+
 	private static class MenuItem {
-		int menuOffResId, menuOnResId;
-		OnClickListener clickEvent;
-		MenuItem(int menuOnResId, int menuOffResId, OnClickListener clickEvent) {
+		final int menuOffResId, menuOnResId;
+		final ClickEvent clickEvent;
+		MenuItem(int menuOnResId, int menuOffResId, ClickEvent clickEvent) {
 			this.menuOffResId = menuOffResId;
 			this.menuOnResId = menuOnResId;
 			this.clickEvent = clickEvent;
 		}
+	}
+
+	private static interface ClickEvent {
+		void onClick();
 	}
 
 	private BaseActivity getActivity() {
