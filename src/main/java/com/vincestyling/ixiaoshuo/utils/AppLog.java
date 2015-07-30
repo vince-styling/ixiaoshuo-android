3f54dee0bcf0cf000e97d622803022030b0276be
vince-styling <lingyunxiao@qq.com>
2015-07-30 17:24:38 +0800

    prevent any Log errors.

--- src/main/java/com/vincestyling/ixiaoshuo/utils/AppLog.java
+++ src/main/java/com/vincestyling/ixiaoshuo/utils/AppLog.java
@@ -6,51 +6,98 @@ public class AppLog {
     private static final String TAG = "ixiaoshuo_console";
 
     public static void d(String msg) {
-        Log.d(TAG, msg);
+        try {
+            Log.d(TAG, msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void d(String msg, Throwable tr) {
-        Log.d(TAG, msg, tr);
+        try {
+            Log.d(TAG, msg, tr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void i(String msg) {
-        Log.i(TAG, msg);
+        try {
+            Log.i(TAG, msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void i(String msg, Throwable tr) {
-        Log.i(TAG, msg, tr);
+        try {
+            Log.i(TAG, msg, tr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void e(String msg) {
-        Log.e(TAG, msg);
+        try {
+            Log.e(TAG, msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void e(String format, Object... args) {
-        Log.e(TAG, String.format(format, args));
+        try {
+            Log.e(TAG, String.format(format, args));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void e(String msg, Throwable tr) {
-        Log.e(TAG, msg, tr);
+        try {
+            Log.e(TAG, msg, tr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void e(Throwable tr) {
-        Log.e(TAG, tr.getMessage(), tr);
+        try {
+            Log.e(TAG, tr.getMessage(), tr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void w(String msg) {
-        Log.w(TAG, msg);
+        try {
+            Log.w(TAG, msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void w(String msg, Throwable tr) {
-        Log.w(TAG, msg, tr);
+        try {
+            Log.w(TAG, msg, tr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void v(String msg) {
-        Log.v(TAG, msg);
+        try {
+            Log.v(TAG, msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void v(String msg, Throwable tr) {
-        Log.v(TAG, msg, tr);
+        try {
+            Log.v(TAG, msg, tr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
